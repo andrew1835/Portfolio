a823ff6827c17e8bc5909c69b439f3e65b2c98e5
Andrew Anthony <a.anthony1835@gmail.com>
2021-04-06 15:55:09 -0700

    navbar is now sticky once your scroll past it. Fixed font issues and spacing of section divs

--- script.js
+++ script.js
@@ -7,3 +7,18 @@
 //     hello.style.color = "red"
 // }
 
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        //if you hard code, then use console
+        //.log to determine when you want the 
+        //nav bar to stick.  
+        console.log($(window).innerHeight())
+        if ($(window).scrollTop() > $(window).innerHeight()) {
+            $('#navBar').addClass('navbar-fixed');
+        }
+        if ($(window).scrollTop() < $(window).innerHeight()) {
+            $('#navBar').removeClass('navbar-fixed');
+        }
+    });
+});
