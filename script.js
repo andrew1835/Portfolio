682372e7bf9faa1485d62ecb84f39200ff1823d6
Andrew Anthony <a.anthony1835@gmail.com>
2021-04-15 18:40:46 -0700

    figured out how to create a carousel. Pseudo coded it. Now just have to make it work, and after that's done, have to put it inside the modal

--- script.js
+++ script.js
@@ -90,3 +90,25 @@ window.onclick = function (event) {
 
 //     });
 // })
+
+// !(function (d) {
+//     var itemClassName = "carousel__photo";
+//     items = d.getElementsByClassName(itemClassName),
+//         totalItems = items.length,
+//         slide = 0,
+//         moving = true;
+//     function setInitialClasses() {
+//         // Targets the previous, current, and next items
+//         // This assumes there are at least three items.
+//         items[totalItems - 1].classList.add("prev");
+//         items[0].classList.add("active");
+//         items[1].classList.add("next");
+//     }
+//     // Set event listeners
+//     function setEventListeners() {
+//         var next = d.getElementsByClassName('carousel__button--next')[0],
+//             prev = d.getElementsByClassName('carousel__button--prev')[0];
+//         next.addEventListener('click', moveNext);
+//         prev.addEventListener('click', movePrev);
+//     }
+// }(document));
