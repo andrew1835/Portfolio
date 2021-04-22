07e1c760ee619e1169eb61dd214823a892136233
Andrew Anthony <a.anthony1835@gmail.com>
2021-04-22 16:17:18 -0700

    the carousel now functions inside the modal, however, when having multiple modals, the X button isn't working.

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-// const hello = document.getElementById("ex")
-// hello.onclick = function () {
-//     myFunction()
-// }
-
-// function myFunction() {
-//     hello.style.color = "red"
-// }
 
 var modal = document.getElementById("modal1")
+var modal2 = document.getElementById("modal2")
 var modalClose = document.getElementById("modalWindow")
-var close = document.getElementById("xIcon")
+var close = document.getElementsByClassName("xIcon")
 
 $(document).ready(function () {
 
@@ -32,6 +25,12 @@ function showModal() {
     $("#content").addClass('bodyBlur');
 }
 
+function showModal1() {
+    document.getElementById("modal2").style.display = "block"
+    $("#content").addClass('bodyBlur');
+}
+
+
 
 window.onclick = function (event) {
     if (event.target == modal || event.target == close) {
@@ -40,75 +39,70 @@ window.onclick = function (event) {
     }
 }
 
-
-// let count = 0
-// $('.card').click(function () {
-//     //if you hard code, then use console
-//     //.log to determine when you want the 
-//     //nav bar to stick. 
-//     count++
-//     $('.card').addClass('cardModal')
-//     $('.card').removeClass('card')
-//     console.log(count)
-
-
-//     // if ($(window).scrollTop() > $(window).innerHeight()) {
-//     //     $('#navBar').addClass('navbar-fixed');
-//     // }
-//     // if ($(window).scrollTop() < $(window).innerHeight()) {
-//     //     $('#navBar').removeClass('navbar-fixed');
-//     // }
-// });
-
-// $('.cardModal').click(function () {
-//     //if you hard code, then use console
-//     //.log to determine when you want the 
-//     //nav bar to stick. 
-//     count--
-//     $('.cardModal').addClass('card')
-//     $('.cardModal').removeClass('cardModal')
-//     console.log(count)
-
-
-//     // if ($(window).scrollTop() > $(window).innerHeight()) {
-//     //     $('#navBar').addClass('navbar-fixed');
-//     // }
-//     // if ($(window).scrollTop() < $(window).innerHeight()) {
-//     //     $('#navBar').removeClass('navbar-fixed');
-//     // }
-// });
-
-
-
-// $(document).ready(function () {
-
-//     $('.card').click(function () {
-//         if ($('.card').clicked == true) {
-//             $('.card').addClass('cardModal')
-//         }
-
-
-//     });
-// })
-
-// !(function (d) {
-//     var itemClassName = "carousel__photo";
-//     items = d.getElementsByClassName(itemClassName),
-//         totalItems = items.length,
-//         slide = 0,
-//         moving = true;
-//     function setInitialClasses() {
-//         // Targets the previous, current, and next items
-//         // This assumes there are at least three items.
-//         items[totalItems - 1].classList.add("prev");
-//         items[0].classList.add("active");
-//         items[1].classList.add("next");
+// window.onclick = function (event) {
+//     if (event.target == modal2 || event.target == close) {
+//         modal2.style.display = "none"
+//         $("#content").removeClass('bodyBlur');
 //     }
-//     // Set event listeners
-//     function setEventListeners() {
-//         var next = d.getElementsByClassName('carousel__button--next')[0],
-//             prev = d.getElementsByClassName('carousel__button--prev')[0];
-//         next.addEventListener('click', moveNext);
-//         prev.addEventListener('click', movePrev);
-//     }
-// }(document));
+// }
+
+
+
+
+
+
+!(function (document) {
+
+    var leftArrow = document.getElementById("leftArrow")
+    var rightArrow = document.getElementById("rightArrow")
+    var photos = document.getElementsByClassName("photo")
+    var photoLength = photos.length
+    var currentSlide = 0
+    console.log("number of photos: " + photoLength)
+
+
+    rightArrow.onclick = function goRight() {
+        if (currentSlide < photoLength - 1) {
+            photos[currentSlide].classList.remove("active")
+            currentSlide++
+            console.log(currentSlide)
+            choosePhoto()
+        }
+        else {
+            photos[currentSlide].classList.remove("active")
+            currentSlide = 0
+            console.log(currentSlide)
+            choosePhoto()
+        }
+
+    }
+
+    leftArrow.onclick = function goLeft() {
+        if (currentSlide < 1) {
+            photos[currentSlide].classList.remove("active")
+            currentSlide = photoLength - 1
+            console.log(currentSlide)
+            choosePhoto()
+        }
+        else {
+            photos[currentSlide].classList.remove("active")
+            currentSlide--
+            console.log(currentSlide)
+            choosePhoto()
+        }
+
+
+    }
+
+    function choosePhoto() {
+        photos[currentSlide].classList.add("active")
+    }
+
+    choosePhoto()
+
+
+
+
+
+
+}(document));
